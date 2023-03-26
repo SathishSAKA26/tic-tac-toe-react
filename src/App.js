50630e4e0873665ee4c36885d1da796a4a9654c0
sathish <2000sathishkumar.d@gmail.com>
2023-03-26 23:12:44 +0530

    start the code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <h1>hello</h1>;
-}
+import React from "react";
+import Game from "./component/Game";
+import Board from "./component/Board";
+import Square from "./component/Square";
+
+const App = () => {
+  return (
+    <div>
+      <Game />
+    </div>
+  );
+};
 
 export default App;
